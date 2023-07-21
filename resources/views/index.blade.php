36b5cb5a156710c931a7b96d0a388ed029968a1a
Peagah <peagahvieira2003@gmail.com>
2023-07-21 08:23:04 -0300

    feat: Favicon create

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -5,8 +5,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Agendamento - Barbershop</title>
     <link rel="stylesheet" href="css/style.css">
+    <link rel="shortcut icon" href="favicon.png" type="image/x-icon">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/flowbite/1.6.3/flowbite.min.css" rel="stylesheet" />
 
     @vite(['resources/css/app.css', 'resources/js/app.js'])
